3449f3746b6bbab95c32a6929b8583669c1d751c
Abraham WG <abrahamwg123@gmail.com>
2022-04-11 21:24:15 +0700

    Success

--- app.py
+++ app.py
@@ -1,34 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import math
 import numpy
 
 app = Flask(__name__, template_folder='template',static_folder='static')
 
-Ts = int(21)
-Tb = int(37)
-
 @app.route("/", methods=["POST", "GET"])
-def index():
-    return render_template("index.html")
 
-@app.route("/", methods=["GET", "POST"])
-def getvalue():
-    Tt = request.form.getlist("T(t)", type=int) 
-    T0 = request.form.getlist("T0", type=int)
-    t = request.form.getlist("t", type=int)
-    Ts = request.form.getlist("Ts", type=int)
+def calculate():
+    x1=""
+    if request.method=='POST' and "T(t)" in request.form and "T0" in request.form and "t" in request.form and "Ts" in request.form:
+        Tt = float(request.form.get("T(t)"))
+        T0 = float(request.form.get("T0"))
+        t = float(request.form.get("t"))
+        Ts = float(request.form.get("Ts"))
+        Tb = 37
 
-    k = (math.log((Tt-Ts)/(T0-Ts)))
-    k1 = k/t*-1
+        k = (math.log((Tt-Ts)/(T0-Ts)))
+        k1 = k/t*-1
 
-    x = (math.log((T0-Ts)/(Tb-Ts)))
-    x1 = x/k1*-1ç
-
-    final = {}
-    final['status'] = x1
-
-    return jsonify(x1)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    
+        x = (math.log((T0-Ts)/(Tb-Ts)))
+        x1 = round(x/k1*-1)
+    return render_template('index.html',x1=x1)
